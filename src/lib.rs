80a32eebdc8eadcb4a6a67ef1fba87b67e178ac7
Bryn Bayliss <brynbayliss@hotmail.com>
2019-10-11 08:31:29 +0100

    errors to stdout

--- src/lib.rs
+++ src/lib.rs
@@ -6,7 +6,6 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.filename)?;
 
     let results = if config.case_sensitive {
-        println!("{}", config.case_sensitive);
         search(&config.query, &contents)
     } else {
         search_case_insensitive(&config.query, &contents)
